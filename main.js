124ac3a9033269288a15ae729c2c4a7f3d7e558e
Stephen Mitchell <stephenmitchell@stephens-mbp.attlocal.net>
2017-05-26 22:44:25 -0400

    email

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var port = process.env.PORT || 8090;
 var app = express();
+
 app.use(express.static('app'))
 
-app.get('/', function(request, response) {
+app.get('/', function(req, res) {
     response.sendfile(__dirname + '/app/index.html');
 });
 
-app.listen(port,function(){
-	console.log("RUNNING");
+
+
+app.listen(port, function() {
+    console.log("RUNNING");
 })
