3c001f5c30b994b9369f46201891a815eb4d0957
Stephen Mitchell <stephenmitchell@stephens-mbp.attlocal.net>
2017-05-30 13:36:19 -0400

    added hover feature

--- app/js/controllers/TableController.js
+++ app/js/controllers/TableController.js
@@ -19,10 +19,73 @@
                 })
             })();
 
-
             $scope.isActive = function(route) {
                 return route === $location.path();
             }
+
+            //Marvel Hover
+
+            $('.marvelSite').mouseover(function() {
+                  $('.marvelPop').removeClass("marvelPopHidden");
+                });
+
+            $('.marvelSite').mouseout(function() {
+                  $('.marvelPop').addClass("marvelPopHidden");
+                });
+
+
+            //JamSesh Hover
+
+            $('.jamSite').mouseover(function() {
+                  $('.jamPop').removeClass("jamPopHidden");
+                });
+
+            $('.jamSite').mouseout(function() {
+                  $('.jamPop').addClass("jamPopHidden");
+                });
+
+            //todo Hover
+
+            $('.todoSite').mouseover(function() {
+                  $('.todoPop').removeClass("todoPopHidden");
+                });
+
+            $('.todoSite').mouseout(function() {
+                  $('.todoPop').addClass("todoPopHidden");
+                });
+
+            //Shopular Hover
+
+            $('.shopularSite').mouseover(function() {
+                  $('.shopularPop').removeClass("shopularPopHidden");
+                });
+
+            $('.shopularSite').mouseout(function() {
+                  $('.shopularPop').addClass("shopularPopHidden");
+                });
+
+            //Insta Hover
+
+            $('.instagramSite').mouseover(function() {
+                  $('.instagramPop').removeClass("instagramPopHidden");
+                });
+
+            $('.instagramSite').mouseout(function() {
+                  $('.instagramPop').addClass("instagramPopHidden");
+                });
+
+            //Blog Hover
+
+            $('.blogSite').mouseover(function() {
+                  $('.blogPop').removeClass("blogPopHidden");
+                });
+
+            $('.blogSite').mouseout(function() {
+                  $('.blogPop').addClass("blogPopHidden");
+                });
+
+
+            
 
 
 
